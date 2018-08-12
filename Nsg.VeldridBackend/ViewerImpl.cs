958c6e690c5f3aae94bf6a75e1e86d34271989bb
seanspicer <sean.spicer@reveal-energy.com>
2018-08-12 11:21:22 -0500

    Bring a window up!

--- Nsg.VeldridBackend/ViewerImpl.cs
+++ Nsg.VeldridBackend/ViewerImpl.cs
@@ -1,9 +1,15 @@
 ﻿using Nsg.Core.Interfaces;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
 
 namespace Nsg.VeldridBackend
 {
     public class ViewerImpl : IViewerImpl
     {
+        private WindowCreateInfo _windowCI;
+        private GraphicsDevice _graphicsDevice;
+        
         public ViewerImpl()
         {
             
@@ -11,12 +17,32 @@ namespace Nsg.VeldridBackend
 
         public void CreateWindow()
         {
-            throw new System.NotImplementedException();
+            _windowCI = new WindowCreateInfo()
+            {
+                X = 100,
+                Y = 100,
+                WindowWidth = 960,
+                WindowHeight = 540,
+                WindowTitle = "Veldrid Tutorial"
+            };
         }
 
         public void Show()
         {
-            throw new System.NotImplementedException();
+            Sdl2Window window = VeldridStartup.CreateWindow(ref _windowCI);
+            _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window);
+            
+            while (window.Exists)
+            {
+                window.PumpEvents();
+            }
+
+            DisposeResources();
+        }
+        
+        private void DisposeResources()
+        {
+            _graphicsDevice.Dispose();
         }
     }
 }
