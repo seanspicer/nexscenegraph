a814c1496eb4c18bf4233e1a8b8f9aa499237ee2
seanspicer <sean.spicer@reveal-energy.com>
2018-08-12 08:30:27 -0500

    Construct Project Structure

---
+++ HelloNsg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloNsg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
