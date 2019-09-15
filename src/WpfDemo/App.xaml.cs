95f3d0b5ac2680f89a01c499761e64d51c0ebc45
Sean Spicer <sean.spicer@reveal-energy.com>
2019-09-15 11:58:04 +0300

    Add Wpf Demo Project

---
+++ src/WpfDemo/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ src/WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
