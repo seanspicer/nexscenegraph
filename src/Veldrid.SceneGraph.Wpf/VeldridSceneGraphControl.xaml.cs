08a7e060b25218e853731137ae9feec5bce16597
Sean Spicer <sean.spicer@reveal-energy.com>
2019-09-16 18:12:03 +0300

    Create Veldrid.SceneGraph.Wpf library

---
+++ src/Veldrid.SceneGraph.Wpf/VeldridSceneGraphControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Veldrid.SceneGraph.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class VeldridSceneGraphControl : UserControl
+    {
+        private Application _app;
+        private VeldridComponent _veldridControl;
+        private Window _myWindow;
+        
+        public VeldridSceneGraphControl()
+        {
+            InitializeComponent();
+        }
+
+        private void On_UIReady(object sender, EventArgs e)
+        {
+            _app = Application.Current;
+            _myWindow = _app.MainWindow;
+            _veldridControl = new VeldridComponent();
+            ControlHostElement.Child = _veldridControl;
+        }
+    }
+}
--- src/WpfDemo/MainWindow.xaml.cs
+++ src/WpfDemo/MainWindow.xaml.cs
@@ -8,24 +8,9 @@ namespace WpfDemo
     /// </summary>
     public partial class MainWindow
     {
-        private Application _app;
-        private IntPtr _hwndListBox;
-        private int _itemCount;
-        private VeldridComponent _veldridControl;
-        private Window _myWindow;
-        private int _selectedItem;
-        
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void On_UIReady(object sender, EventArgs e)
-        {
-            _app = Application.Current;
-            _myWindow = _app.MainWindow;
-            _veldridControl = new VeldridComponent();
-            ControlHostElement.Child = _veldridControl;
         }
     }
 }
