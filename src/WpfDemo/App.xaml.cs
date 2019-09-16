411efe9bd8b130d07e42f352c893a64fb95303c5
Sean Spicer <sean.spicer@reveal-energy.com>
2019-09-16 11:47:06 +0300

    Create WpfDemo based on Eric's gist - need to move this to it's own project.

--- src/WpfDemo/App.xaml.cs
+++ src/WpfDemo/App.xaml.cs
@@ -1,9 +1,57 @@
-﻿namespace WpfDemo
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfDemo
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DisableWPFTabletSupport();
+        }
+        
+        public static void DisableWPFTabletSupport()
+        {
+            // Get a collection of the tablet devices for this window.    
+            TabletDeviceCollection devices = System.Windows.Input.Tablet.TabletDevices;
+
+            if (devices.Count > 0)
+            {
+                // Get the Type of InputManager.  
+                Type inputManagerType = typeof(System.Windows.Input.InputManager);
+
+
+                // Call the StylusLogic method on the InputManager.Current instance.  
+                object stylusLogic = inputManagerType.InvokeMember("StylusLogic",
+                    BindingFlags.GetProperty | BindingFlags.Instance | 
+                    BindingFlags.NonPublic,
+                    null, InputManager.Current, null);
+
+
+                if (stylusLogic != null)
+                {
+                    // Get the type of the stylusLogic returned 
+                    // from the call to StylusLogic.  
+                    Type stylusLogicType = stylusLogic.GetType();
+
+
+                    // Loop until there are no more devices to remove.  
+                    while (devices.Count > 0)
+                    {
+                        // Remove the first tablet device in the devices collection.  
+                        stylusLogicType.InvokeMember("OnTabletRemoved",
+                            BindingFlags.InvokeMethod | 
+                            BindingFlags.Instance | BindingFlags.NonPublic,
+                            null, stylusLogic, new object[] { (uint)0 });
+                    }
+                }
+            }
+        }
     }
 }
--- src/WpfDemo/MainWindow.xaml.cs
+++ src/WpfDemo/MainWindow.xaml.cs
@@ -1,13 +1,31 @@
-﻿namespace WpfDemo
+﻿using System;
+using System.Windows;
+
+namespace WpfDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private Application _app;
+        private IntPtr _hwndListBox;
+        private int _itemCount;
+        private VeldridComponent _veldridControl;
+        private Window _myWindow;
+        private int _selectedItem;
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void On_UIReady(object sender, EventArgs e)
+        {
+            _app = Application.Current;
+            _myWindow = _app.MainWindow;
+            _veldridControl = new VeldridComponent();
+            ControlHostElement.Child = _veldridControl;
         }
     }
 }
