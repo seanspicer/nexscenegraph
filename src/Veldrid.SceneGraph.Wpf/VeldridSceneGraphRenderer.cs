c662c36a431dbef20f9feed25ac4bd6bc1d22f46
Sean Spicer <sean.spicer@reveal-energy.com>
2019-09-24 12:51:41 -0500

    Blit offscreen buffer to render target.

--- src/Veldrid.SceneGraph.Wpf/VeldridSceneGraphRenderer.cs
+++ src/Veldrid.SceneGraph.Wpf/VeldridSceneGraphRenderer.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Windows;
 using SharpDX;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
 using Veldrid.SceneGraph.Wpf.Element;
 using Veldrid.Utilities;
+using Buffer = SharpDX.Direct3D11.Buffer;
+using Device = SharpDX.DXGI.Device;
 
 namespace Veldrid.SceneGraph.Wpf
 {
@@ -14,6 +19,8 @@ namespace Veldrid.SceneGraph.Wpf
         
         private Framebuffer _offscreenFB;
         private Texture _offscreenColor;
+
+        private Fence _fence;
         
         private DisposeCollectorResourceFactory _factory;
         
@@ -26,8 +33,10 @@ namespace Veldrid.SceneGraph.Wpf
             
             _factory = new DisposeCollectorResourceFactory(_gd.ResourceFactory);
             _cl = _gd.ResourceFactory.CreateCommandList();
+            _fence = _factory.CreateFence(false);
         }
-
+        
+        
         protected override void Attach()
         {
             if (Renderer == null)
@@ -49,7 +58,8 @@ namespace Veldrid.SceneGraph.Wpf
 
             _offscreenColor = _factory.CreateTexture(TextureDescription.Texture2D(
                 width, height, 1, 1,
-                PixelFormat.R8_G8_B8_A8_UNorm, TextureUsage.RenderTarget | TextureUsage.Sampled));
+                PixelFormat.B8_G8_R8_A8_UNorm, TextureUsage.RenderTarget | TextureUsage.Sampled));
+            
             Texture offscreenDepth = _factory.CreateTexture(TextureDescription.Texture2D(
                 width, height, 1, 1, PixelFormat.R16_UNorm, TextureUsage.DepthStencil));
             _offscreenFB = _factory.CreateFramebuffer(new FramebufferDescription(offscreenDepth, _offscreenColor));
@@ -57,6 +67,7 @@ namespace Veldrid.SceneGraph.Wpf
 
         public override void RenderCore(DrawEventArgs args)
         {
+            _fence.Reset();
             _cl.Begin();
             _cl.SetFramebuffer(_offscreenFB);
             Random r = new Random();
@@ -68,14 +79,15 @@ namespace Veldrid.SceneGraph.Wpf
             // Do your rendering here (or call a subclass, etc.)
 
             _cl.End();
-            _gd.SubmitCommands(_cl);
+            _gd.SubmitCommands(_cl, _fence);
             
             // Now just need to copy the offscreen buffer to the render target (I think!)
             var deviceTexture = (SharpDX.Direct3D11.Texture2D) _offscreenColor.GetType().GetProperty("DeviceTexture")?.GetValue(_offscreenColor);
             
+            Renderer.Device.ImmediateContext.ClearRenderTargetView(Renderer.RenderTargetView, new Color4(0.6f, 0, 0, 1));
+
+            _gd.WaitForFence(_fence);
             deviceTexture?.Device.ImmediateContext.CopyResource(deviceTexture, Renderer.RenderTarget);
-            
-            Renderer.Device.ImmediateContext.ClearRenderTargetView(Renderer.RenderTargetView, new Color4(0.6f, 0, 0, 0));
             
         }
     }
