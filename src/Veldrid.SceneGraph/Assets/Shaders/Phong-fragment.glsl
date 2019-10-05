53c275af4cfa9f0afa113808f67f70330ce07ccd
seanspicer <sean.spicer@reveal-energy.com>
2019-10-05 10:56:11 -0500

    Continue Optimization...reduce variables passed.

--- src/Veldrid.SceneGraph/Assets/Shaders/Phong-fragment.glsl
+++ src/Veldrid.SceneGraph/Assets/Shaders/Phong-fragment.glsl
@@ -1,28 +1,22 @@
 #version 450
 
-struct LightSourceOut {
+struct LightData {
+
+    // Color Products
+    vec4 AmbientColor;
+    vec4 DiffuseColor;
+    vec4 SpecularColor;
     
-    vec4 AmbientColor;
-    vec4 DiffuseColor;
-    vec4 SpecularColor;
-    vec4 Position;
-};
-
-struct MaterialDescOut {
-
-    vec4 AmbientColor;
-    vec4 DiffuseColor;
-    vec4 SpecularColor;
-    vec4 EyeDirection_cameraspace;
+    // Values
+    vec4 LightVec;      // Camera Space
+    vec4 EyeDirection;  // Camera Space
+    vec4 Constants;     // [0] = Power; [1] = SpecularPower; [2] = AttenuationConstant
 
 };
 
 layout(location = 0) in vec3 fsin_normal;
-layout(location = 1) in vec3 fsin_color;
-layout(location = 2) in vec3 fsin_eyePos;
-layout(location = 3) in vec3 fsin_lightVec;
-layout(location = 4) flat in LightSourceOut fsin_lightSourceOut; 
-layout(location = 12) flat in MaterialDescOut fsin_materialDescOut;
+layout(location = 1) in vec3 fsin_eyeDir;
+layout(location = 2) flat in LightData fsin_lightData; 
 
 layout(location = 0) out vec4 fsout_color;
 
@@ -30,20 +24,19 @@ void main()
 {
     // Inputs
     vec3 n = normalize(fsin_normal);
-    vec3 l = normalize(fsin_lightVec);
-    vec3 e = normalize(fsin_eyePos);
+    vec3 l = normalize(fsin_lightData.LightVec.xyz);
     
-    vec3 AmbientColor = fsin_materialDescOut.AmbientColor.xyz;
-    vec3 DiffuseColor = fsin_materialDescOut.DiffuseColor.xyz;
-    vec3 SpecularColor = fsin_materialDescOut.SpecularColor.xyz;
+    vec3 AmbientColor = fsin_lightData.AmbientColor.xyz;
+    vec3 DiffuseColor = fsin_lightData.DiffuseColor.xyz;
+    vec3 SpecularColor = fsin_lightData.SpecularColor.xyz;
 
-    float LightPower = fsin_lightSourceOut.AmbientColor.w;
-    float SpecularPower = fsin_materialDescOut.AmbientColor.w;
-    float AttenuationConstant = fsin_lightSourceOut.DiffuseColor.w;
+    float LightPower = fsin_lightData.Constants.x;
+    float SpecularPower = fsin_lightData.Constants.y;
+    float AttenuationConstant = fsin_lightData.Constants.z;
     
     // Compute the Light Power and Attenuation
     vec3 LightPowerVec = vec3(LightPower, LightPower, LightPower);
-    float distance = distance(vec3(0,0,0), fsin_lightVec);
+    float distance = distance(vec3(0,0,0), fsin_lightData.LightVec.xyz);
     float oneOverDistanceAtten = 1.0f/(pow(distance, AttenuationConstant));
     vec3 Attenuation = vec3(oneOverDistanceAtten, oneOverDistanceAtten, oneOverDistanceAtten);
 
@@ -52,7 +45,7 @@ void main()
     vec3 CosThetaVec = vec3(cosTheta, cosTheta, cosTheta);
     
     // Eye vector (towards the camera)
-    vec3 E = normalize(fsin_materialDescOut.EyeDirection_cameraspace.xyz);
+    vec3 E = normalize(fsin_eyeDir);
     
     // Direction in which the triangle reflects the light
     vec3 R = reflect(-l,n);
--- src/Veldrid.SceneGraph/Assets/Shaders/Phong-vertex.glsl
+++ src/Veldrid.SceneGraph/Assets/Shaders/Phong-vertex.glsl
@@ -22,20 +22,17 @@ struct MaterialDescStruct {
     vec4 Padding1;
 };
 
-struct LightSourceOut {
+struct LightData {
+
+    // Color Products
+    vec4 AmbientColor;
+    vec4 DiffuseColor;
+    vec4 SpecularColor;
     
-    vec4 AmbientColor;
-    vec4 DiffuseColor;
-    vec4 SpecularColor;
-    vec4 Position;
-};
-
-struct MaterialDescOut {
-
-    vec4 AmbientColor;
-    vec4 DiffuseColor;
-    vec4 SpecularColor;
-    vec4 EyeDirection_cameraspace;
+    // Values
+    vec4 LightVec;
+    vec4 EyeDirection;  // Camera Space
+    vec4 Constants;     // [0] = Power; [1] = SpecularPower; [2] = AttenuationConstant
 
 };
 
@@ -70,11 +67,8 @@ layout(location = 2) in vec3 Color;
 layout(location = 3) in vec3 Normal;
 
 layout(location = 0) out vec3 fsin_normal;
-layout(location = 1) out vec3 fsin_color;
-layout(location = 2) out vec3 fsin_eyePos;
-layout(location = 3) out vec3 fsin_lightVec;
-layout(location = 4) out LightSourceOut fsin_lightSourceOut; 
-layout(location = 12) out MaterialDescOut fsin_materialDescOut;
+layout(location = 1) out vec3 fsin_eyeDir;
+layout(location = 2) out LightData fsin_lightData; 
 
 void main()
 {
@@ -96,25 +90,19 @@ void main()
     
     vec3 Normal_cameraspace = ( field_View * transpose(inverse(field_Model)) * vec4(Normal,0)).xyz;
     
-     
-    
-    fsin_color = Color;
-    fsin_eyePos = vec3(0,0,0);
+    //fsin_color = Color;
+    fsin_eyeDir = EyeDirection_cameraspace;
     fsin_normal = Normal_cameraspace;
-    fsin_lightVec = LightDirection_cameraspace;
     
-    fsin_lightSourceOut.AmbientColor = vec4(lightSource.AmbientColor, lightSource.LightPower);
-    fsin_lightSourceOut.DiffuseColor = vec4(lightSource.DiffuseColor, lightSource.AttenuationConstant);
-    fsin_lightSourceOut.SpecularColor = vec4(lightSource.SpecularColor, lightSource.IsHeadlight);
-    fsin_lightSourceOut.Position = lightSource.Position;
+    fsin_lightData.AmbientColor = vec4(materialDesc.AmbientColor, 1.0f);
+    fsin_lightData.DiffuseColor = vec4(materialDesc.DiffuseColor, 1.0f);
+    fsin_lightData.SpecularColor = vec4(materialDesc.SpecularColor, 1.0f);
     
-    fsin_materialDescOut.AmbientColor = vec4(materialDesc.AmbientColor, materialDesc.Shininess);
-    fsin_materialDescOut.DiffuseColor = vec4(materialDesc.DiffuseColor, materialDesc.MaterialOverride);
-    fsin_materialDescOut.SpecularColor = vec4(materialDesc.SpecularColor, 1.0f);
-    fsin_materialDescOut.EyeDirection_cameraspace = vec4(EyeDirection_cameraspace, 0.0f);
+    fsin_lightData.LightVec = vec4(LightDirection_cameraspace,1.0f);
+    fsin_lightData.Constants = vec4(lightSource.LightPower, materialDesc.Shininess, lightSource.AttenuationConstant, 1.0f);
     
     if(0 == materialDesc.MaterialOverride) {
-       fsin_materialDescOut.AmbientColor = vec4(fsin_color*lightSource.AmbientColor, materialDesc.Shininess);
-       fsin_materialDescOut.DiffuseColor = vec4(fsin_color*lightSource.DiffuseColor, materialDesc.MaterialOverride);
+       fsin_lightData.AmbientColor = vec4(Color*lightSource.AmbientColor, 1.0);
+       fsin_lightData.DiffuseColor = vec4(Color*lightSource.DiffuseColor, 1.0);
     }
 }
