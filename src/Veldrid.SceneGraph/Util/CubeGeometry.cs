f1f6c099762ac3a91515b8595b74d419305ca1c6
Sean Spicer <sean.spicer@reveal-energy.com>
2019-10-20 13:28:49 -0500

    Update Copyright

--- src/Veldrid.SceneGraph/Util/CubeGeometry.cs
+++ src/Veldrid.SceneGraph/Util/CubeGeometry.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 using Veldrid.SceneGraph.VertexTypes;
 
 //
-// Copyright 2018 Sean Spicer 
+// Copyright 2018-2019 Sean Spicer 
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
