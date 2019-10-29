c6c176bc7841c1ecc390f520cc7874436f6f25f1
Sean Spicer <sean.spicer@reveal-energy.com>
2019-10-29 16:40:09 -0500

    Start trying to fix extrusion error.

--- src/Examples.Common/PathExampleScene.cs
+++ src/Examples.Common/PathExampleScene.cs
@@ -1,10 +1,17 @@
+using System;
+using System.IO;
+using System.Linq;
 using System.Numerics;
 using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 using Veldrid;
 using Veldrid.SceneGraph;
 using Veldrid.SceneGraph.PipelineStates;
 using Veldrid.SceneGraph.Util.Shape;
 using Veldrid.SceneGraph.VertexTypes;
+using Path = Veldrid.SceneGraph.Util.Shape.Path;
 
 namespace Examples.Common
 {
@@ -14,6 +21,8 @@ namespace Examples.Common
         {
             var root = Group.Create();
 
+            var pathData = GetPathData();
+            
             var path = Path.Create(new Vector3[]
             {
                 new Vector3(0.0f,  1.0f, 0.0f),
@@ -22,9 +31,13 @@ namespace Examples.Common
                 new Vector3(2.0f, -2.0f, 2.0f), 
             });
 
+            var start = 70;
+            path = Path.Create(pathData.ToList().GetRange(start, 3).ToArray());
+            //path = Path.Create(pathData);
+
             var hints = TessellationHints.Create();
             hints.SetDetailRatio(4f);
-            hints.SetRadius(0.1f);
+            hints.SetRadius(1f);
             var pathDrawable = ShapeDrawable<Position3Texture2Color3Normal3>.Create(path, hints);
             
             var redMaterial = PhongMaterial.Create(
@@ -49,6 +62,40 @@ namespace Examples.Common
 
             root.AddChild(pathGeode);
             return root;
+        }
+
+        static Vector3[] GetPathData()
+        {
+            var dataString = File.ReadAllText(@"C:\Users\sean.spicer.RES\Downloads\badPath.json");
+            var json = JObject.Parse(dataString);
+
+            var vecArray = json["Vector3"] as JArray;
+
+            if (null == vecArray)
+            {
+                return new Vector3[0];
+            }
+
+            var result = new Vector3[vecArray.Count];
+            var idx = 0;
+            foreach (var elt in vecArray)
+            {
+                if (elt is JObject vecData)
+                {
+                    var x = vecData["X"].ToObject<double>();
+                    var y = vecData["Y"].ToObject<double>();
+                    var z = vecData["Z"].ToObject<double>();
+
+                    var vec = new Vector3((float)x,(float)y,(float)z );
+
+                    result[idx] = vec;
+                }
+
+                    
+                ++idx;
+            }
+
+            return result;
         }
     }
 }
