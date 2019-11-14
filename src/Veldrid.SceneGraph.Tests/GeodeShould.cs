619a628058d3d3db2f721addef348f56563045f4
Sean Spicer <sean.spicer@reveal-energy.com>
2019-11-13 23:05:02 -0600

    Add Node Bounding Box and ComputeBound Tests

---
+++ src/Veldrid.SceneGraph.Tests/GeodeShould.cs
@@ -0,0 +1,93 @@
+﻿using System.Numerics;
+using NUnit.Framework;
+
+namespace Veldrid.SceneGraph.Tests
+{
+    [TestFixture]
+    public class GeodeShould
+    {
+        [TestCase]
+        public void ComputeBoundingSphereCorrectlyFor3dGeometry()
+        {
+            var geom = TestGeometryHelpers.Get3dGeometry();
+            var geode = Geode.Create();
+            geode.AddDrawable(geom);
+
+            var bSphere = geode.ComputeBound();
+            
+            Assert.That(bSphere.Center, Is.EqualTo(Vector3.Zero));
+            Assert.That(bSphere.Radius, Is.EqualTo(System.Math.Sqrt(3)).Within(1e-6));
+        }
+        
+        [TestCase]
+        public void ComputeBoundingBoxCorrectlyFor3dGeometry()
+        {
+            var geom = TestGeometryHelpers.Get3dGeometry();
+            var geode = Geode.Create();
+            geode.AddDrawable(geom);
+
+            var bbox = geode.GetBoundingBox();
+            
+            Assert.That(bbox.Center, Is.EqualTo(Vector3.Zero));
+            Assert.That(bbox.Radius, Is.EqualTo(System.Math.Sqrt(3)).Within(1e-6));
+            Assert.That(bbox.Min, Is.EqualTo(-Vector3.One));
+            Assert.That(bbox.Max, Is.EqualTo(Vector3.One));
+        }
+        
+        [TestCase]
+        public void ComputeBoundingSphereCorrectlyForPlaneGeometry()
+        {
+            var geom = TestGeometryHelpers.GetPlaneGeometry();
+            var geode = Geode.Create();
+            geode.AddDrawable(geom);
+
+            var bSphere = geode.ComputeBound();
+            
+            Assert.That(bSphere.Center, Is.EqualTo(Vector3.Zero));
+            Assert.That(bSphere.Radius, Is.EqualTo(System.Math.Sqrt(2)).Within(1e-6));
+        }
+        
+        [TestCase]
+        public void ComputeBoundingBoxCorrectlyForPlaneGeometry()
+        {
+            var geom = TestGeometryHelpers.GetPlaneGeometry();
+            var geode = Geode.Create();
+            geode.AddDrawable(geom);
+
+            var bbox = geode.GetBoundingBox();
+            
+            Assert.That(bbox.Center, Is.EqualTo(Vector3.Zero));
+            Assert.That(bbox.Radius, Is.EqualTo(System.Math.Sqrt(2)).Within(1e-6));
+            Assert.That(bbox.Min, Is.EqualTo(new Vector3(-1, -1, 0)));
+            Assert.That(bbox.Max, Is.EqualTo(new Vector3(1, 1, 0)));
+        }
+        
+        [TestCase]
+        public void ComputeBoundingSphereCorrectlyForUniaxialGeometry()
+        {
+            var geom = TestGeometryHelpers.GetUniaxialGeometry();
+            var geode = Geode.Create();
+            geode.AddDrawable(geom);
+
+            var bSphere = geode.ComputeBound();
+            
+            Assert.That(bSphere.Center, Is.EqualTo(Vector3.Zero));
+            Assert.That(bSphere.Radius, Is.EqualTo(1).Within(1e-6));
+        }
+        
+        [TestCase]
+        public void ComputeBoundingBoxCorrectlyForUniaxialGeometry()
+        {
+            var geom = TestGeometryHelpers.GetUniaxialGeometry();
+            var geode = Geode.Create();
+            geode.AddDrawable(geom);
+
+            var bbox = geode.GetBoundingBox();
+            
+            Assert.That(bbox.Center, Is.EqualTo(Vector3.Zero));
+            Assert.That(bbox.Radius, Is.EqualTo(1).Within(1e-6));
+            Assert.That(bbox.Min, Is.EqualTo(new Vector3(-1, 0, 0)));
+            Assert.That(bbox.Max, Is.EqualTo(new Vector3(1, 0, 0)));
+        }
+    }
+}
---
+++ src/Veldrid.SceneGraph.Tests/GeometryShould.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using Moq;
+using NUnit.Framework;
+using Veldrid.SceneGraph.VertexTypes;
+
+namespace Veldrid.SceneGraph.Tests
+{
+    public static class TestGeometryHelpers
+    {
+        public static IDrawable Get3dGeometry()
+        {
+            var vertices = new Position3Color3[3];
+            vertices[0] = new Position3Color3(new Vector3(-1, -1, -1), Vector3.UnitX);
+            vertices[1] = new Position3Color3(new Vector3(-1, -1, 1), Vector3.UnitX);
+            
+            // Use an extra vertex to be sure we don't introduce an "add" bug
+            vertices[2] = new Position3Color3(new Vector3(1, 1, 1), Vector3.UnitX);
+
+            var geometry = Geometry<Position3Color3>.Create();
+            geometry.IndexData = new[] {0u, 1u, 2u};
+            geometry.VertexData = vertices;
+            
+            var drawElts = DrawElements<Position3Color3>.Create(
+                geometry,
+                PrimitiveTopology.LineStrip,
+                3, 
+                1, 
+                0, 
+                0, 
+                0);
+
+            geometry.PrimitiveSets.Add(drawElts);
+
+            return geometry;
+        }
+        
+        public static IDrawable GetPlaneGeometry()
+        {
+            var vertices = new Position3Color3[3];
+            vertices[0] = new Position3Color3(new Vector3(-1, -1, 0), Vector3.UnitX);
+            vertices[1] = new Position3Color3(new Vector3(1, 1, 0), Vector3.UnitX);
+            
+            var geometry = Geometry<Position3Color3>.Create();
+            geometry.IndexData = new[] {0u, 1u};
+            geometry.VertexData = vertices;
+            
+            var drawElts = DrawElements<Position3Color3>.Create(
+                geometry,
+                PrimitiveTopology.LineStrip,
+                2, 
+                1, 
+                0, 
+                0, 
+                0);
+
+            geometry.PrimitiveSets.Add(drawElts);
+
+            return geometry;
+        }
+        
+        public static IDrawable GetUniaxialGeometry()
+        {
+            var vertices = new Position3Color3[3];
+            vertices[0] = new Position3Color3(new Vector3(-1, 0, 0), Vector3.UnitX);
+            vertices[1] = new Position3Color3(new Vector3(1, 0, 0), Vector3.UnitX);
+            
+            var geometry = Geometry<Position3Color3>.Create();
+            geometry.IndexData = new[] {0u, 1u};
+            geometry.VertexData = vertices;
+            
+            var drawElts = DrawElements<Position3Color3>.Create(
+                geometry,
+                PrimitiveTopology.LineStrip,
+                2, 
+                1, 
+                0, 
+                0, 
+                0);
+
+            geometry.PrimitiveSets.Add(drawElts);
+
+            return geometry;
+        }
+    }
+    
+    [TestFixture]
+    public class GeometryShould
+    {
+        [TestCase]
+        public void ComputeBoundingBoxCorrectlyFor3dGeometry()
+        {
+            var geometry = TestGeometryHelpers.Get3dGeometry();
+
+            var bbox = geometry.GetBoundingBox();
+            
+            Assert.That(bbox.Center, Is.EqualTo(Vector3.Zero));
+            Assert.That(bbox.Radius, Is.EqualTo(System.Math.Sqrt(3)).Within(1e-6));
+            Assert.That(bbox.Min, Is.EqualTo(-Vector3.One));
+            Assert.That(bbox.Max, Is.EqualTo(Vector3.One));
+        }
+        
+        [TestCase]
+        public void ComputeBoundingBoxCorrectlyForPlaneGeometry()
+        {
+            var geometry = TestGeometryHelpers.GetPlaneGeometry();
+
+            var bbox = geometry.GetBoundingBox();
+            
+            Assert.That(bbox.Center, Is.EqualTo(Vector3.Zero));
+            Assert.That(bbox.Radius, Is.EqualTo(System.Math.Sqrt(2)).Within(1e-6));
+            Assert.That(bbox.Min, Is.EqualTo(new Vector3(-1, -1, 0)));
+            Assert.That(bbox.Max, Is.EqualTo(new Vector3(1, 1, 0)));
+        }
+        
+        [TestCase]
+        public void ComputeBoundingBoxCorrectlyForUniaxialGeometry()
+        {
+            var geometry = TestGeometryHelpers.GetUniaxialGeometry();
+
+            var bbox = geometry.GetBoundingBox();
+            
+            Assert.That(bbox.Center, Is.EqualTo(Vector3.Zero));
+            Assert.That(bbox.Radius, Is.EqualTo(1).Within(1e-6));
+            Assert.That(bbox.Min, Is.EqualTo(new Vector3(-1, 0, 0)));
+            Assert.That(bbox.Max, Is.EqualTo(new Vector3(1, 0, 0)));
+        }
+    }
+}
