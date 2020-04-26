78c5756a019fbdaba8d4a335de489e75ee01b58c
Sean Spicer <sean.spicer@reveal-energy.com>
2020-04-26 09:34:03 -0500

    Add Static Transform to Path Extrusion

--- src/Examples.Common/AntiSquishExampleScene.cs
+++ src/Examples.Common/AntiSquishExampleScene.cs
@@ -13,43 +13,52 @@ namespace Examples.Common
         {
             var root = Group.Create();
 
-            var path = Path.Create(new Vector3[]
+            var pathCoords = new Vector3[]
             {
-                new Vector3(0.0f,  0.0f, 0.0f),
-                new Vector3(0.0f,  0.0f, -15.0f), 
-                new Vector3(5.0f,  0.0f, -20.0f),
-                new Vector3(20.0f, 0.0f, -20.0f), 
-            });
-
+                new Vector3(0.0f, 0.0f, 0.0f),
+                new Vector3(0.0f, 0.0f, -15.0f),
+                new Vector3(5.0f, 0.0f, -20.0f),
+                new Vector3(20.0f, 0.0f, -20.0f),
+            };
+            
+            var path = Path.Create(pathCoords);
+            var scaledPath = Path.Create(pathCoords, Matrix4x4.CreateScale(1, 1, 2));
+            
+            
             var hints = TessellationHints.Create();
             hints.SetDetailRatio(4f);
             hints.SetRadius(.1f);
             var pathDrawable = ShapeDrawable<Position3Texture2Color3Normal3>.Create(path, hints);
-
             var pathGeode = Geode.Create();
             pathGeode.AddDrawable(pathDrawable);
+            
+            var scaledPathDrawable = ShapeDrawable<Position3Texture2Color3Normal3>.Create(scaledPath, hints);
+            var scaledPathGeode = Geode.Create();
+            scaledPathGeode.AddDrawable(scaledPathDrawable);
 
             var Xlate = MatrixTransform.Create(Matrix4x4.CreateTranslation(0, 10, 0));
             Xlate.PipelineState = GetPipelineState(1.0f, 0.0f, 0.0f);
             Xlate.PipelineState.RasterizerStateDescription 
                 = new RasterizerStateDescription(FaceCullMode.None, PolygonFillMode.Solid, FrontFace.Clockwise, true, false);
             
-            Xlate.AddChild(pathGeode);
+            Xlate.AddChild(scaledPathGeode);
             
             
             var XForm = MatrixTransform.Create(Matrix4x4.CreateScale(1, 1, 2));
 
-            var antiSquish = AntiSquish.Create();
-            antiSquish.PipelineState = GetPipelineState(0.0f, 1.0f, 0.0f);
-            antiSquish.PipelineState.RasterizerStateDescription 
+            //var antiSquish = AntiSquish.Create();
+            XForm.PipelineState = GetPipelineState(0.0f, 1.0f, 0.0f);
+            XForm.PipelineState.RasterizerStateDescription 
                 = new RasterizerStateDescription(FaceCullMode.None, PolygonFillMode.Solid, FrontFace.Clockwise, true, false);
+            XForm.AddChild(pathGeode);
             
-            antiSquish.AddChild(pathGeode);
+            //antiSquish.AddChild(pathGeode);
             
-            XForm.AddChild(antiSquish);
-            XForm.AddChild(Xlate);
+            //XForm.AddChild(antiSquish);
+            //XForm.AddChild(Xlate);
 
             root.AddChild(XForm);
+            root.AddChild(Xlate);
             return root;
         }
 
