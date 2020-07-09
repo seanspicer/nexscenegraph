f18722d44a8cc14e4bbd6713bffb08071a7fe853
Sean Spicer <sean.spicer@reveal-energy.com>
2020-07-08 22:25:22 -0500

    refactor for opt start

--- src/Examples.Common/LargeSphereCountScene.cs
+++ src/Examples.Common/LargeSphereCountScene.cs
@@ -19,40 +19,42 @@ namespace Examples.Common
             sphereHints.SetDetailRatio(0.2f);
             var sphereGeode = Geode.Create();
             
-            for (var i = 0; i < 500; ++i)
+            var sphereShape = Sphere.Create(Vector3.Zero, 0.5f);
+            
+            var sphereDrawable =
+                ShapeDrawable<Position3Texture2Color3Normal3>.Create(
+                    sphereShape,
+                    sphereHints,
+                    new Vector3[] {new Vector3(1.0f, 0.0f, 0.0f)});
+
+            //sphereGeode.AddDrawable(sphereDrawable);
+
+            var sphereMaterial = PhongMaterial.Create(
+            PhongMaterialParameters.Create(
+                new Vector3(0.0f, 0.0f, 1.0f),
+                new Vector3(0.0f, 0.0f, 1.0f),
+                new Vector3(1.0f, 1.0f, 1.0f),
+                5f),
+            PhongPositionalLight.Create( new Vector4(0, 10, 0, 1),PhongLightParameters.Create(
+                new Vector3(0.1f, 0.1f, 0.1f),
+                new Vector3(1.0f, 1.0f, 1.0f),
+                new Vector3(1.0f, 1.0f, 1.0f),
+                10000f,
+                2)),
+            true);
+
+            sphereDrawable.PipelineState = sphereMaterial.CreatePipelineState();
+            
+            for (var i = 0; i < 5000; ++i)
             {
                 var xPos = (float) random.NextDouble() * 100;
                 var yPos = (float) random.NextDouble() * 100;
-                
-                var sphereShape = Sphere.Create(new Vector3(xPos, yPos, 0.0f), 0.5f);
-                
-                var sphereDrawable =
-                    ShapeDrawable<Position3Texture2Color3Normal3>.Create(
-                        sphereShape,
-                        sphereHints,
-                        new Vector3[] {new Vector3(1.0f, 0.0f, 0.0f)});
 
-                sphereGeode.AddDrawable(sphereDrawable);
+                var xform = MatrixTransform.Create(Matrix4x4.CreateTranslation(xPos, yPos, 0.0f));
+                xform.AddChild(sphereDrawable);
+                root.AddChild(xform);
             }
-            
-            var sphereMaterial = PhongMaterial.Create(
-                PhongMaterialParameters.Create(
-                    new Vector3(0.0f, 0.0f, 1.0f),
-                    new Vector3(0.0f, 0.0f, 1.0f),
-                    new Vector3(1.0f, 1.0f, 1.0f),
-                    5f),
-                PhongPositionalLight.Create( new Vector4(0, 10, 0, 1),PhongLightParameters.Create(
-                    new Vector3(0.1f, 0.1f, 0.1f),
-                    new Vector3(1.0f, 1.0f, 1.0f),
-                    new Vector3(1.0f, 1.0f, 1.0f),
-                    10000f,
-                    2)),
-                true);
-            
-            root.AddChild(sphereGeode);
-            
-            root.PipelineState = sphereMaterial.CreatePipelineState();
-            
+
             return root;
         }
     }
