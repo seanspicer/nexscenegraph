8f83dbfa50b1c4c98cc198d795e33d64e88707c9
Sean Spicer <sean.spicer@reveal-energy.com>
2021-01-19 21:36:43 -0600

    Start work on dynamic update

--- src/Veldrid.SceneGraph/NodeKits/DirectVolumeRendering/SampledVolumeNode.cs
+++ src/Veldrid.SceneGraph/NodeKits/DirectVolumeRendering/SampledVolumeNode.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
+using System.Reactive;
+using System.Xml.Schema;
 using Veldrid.SceneGraph.Math.IsoSurface;
+using Veldrid.SceneGraph.RenderGraph;
 using Veldrid.SceneGraph.Shaders.Standard;
 using Veldrid.SceneGraph.VertexTypes;
 
@@ -10,16 +14,182 @@ namespace Veldrid.SceneGraph.NodeKits.DirectVolumeRendering
     {
         
     }
+
+    class SamplingVolume : IVoxelVolume
+    {
+        public double[,,] Values { get; }
+        public double[,,] XValues { get; }
+        public double[,,] YValues { get; }
+        public double[,,] ZValues { get; }
+
+        private IVoxelVolume _source;
+        
+        public double MinDist { get; private set; }
+        public double MaxDist { get; private set; }
+        
+        internal SamplingVolume(IVoxelVolume source)
+        {
+            _source = source;
+
+            var xdim = _source.XValues.GetLength(0)-1;
+            var ydim = _source.XValues.GetLength(1)-1;
+            var zdim = _source.XValues.GetLength(2)-1;
+            
+            Values = new double[2, 2, 2];
+            XValues = new double[2, 2, 2];
+            YValues = new double[2, 2, 2];
+            ZValues = new double[2, 2, 2];
+            
+            XValues[0, 0, 0] = _source.XValues[0, 0, 0];
+            YValues[0, 0, 0] = _source.YValues[0, 0, 0];
+            ZValues[0, 0, 0] = _source.ZValues[0, 0, 0];
+            
+            XValues[1, 0, 0] = _source.XValues[xdim, 0, 0];
+            YValues[1, 0, 0] = _source.YValues[xdim, 0, 0];
+            ZValues[1, 0, 0] = _source.ZValues[xdim, 0, 0];
+            
+            XValues[0, 1, 0] = _source.XValues[0, ydim, 0];
+            YValues[0, 1, 0] = _source.YValues[0, ydim, 0];
+            ZValues[0, 1, 0] = _source.ZValues[0, ydim, 0];
+            
+            XValues[0, 0, 1] = _source.XValues[0, 0, zdim];
+            YValues[0, 0, 1] = _source.YValues[0, 0, zdim];
+            ZValues[0, 0, 1] = _source.ZValues[0, 0, zdim];
+            
+            XValues[1, 1, 0] = _source.XValues[xdim, ydim, 0];
+            YValues[1, 1, 0] = _source.YValues[xdim, ydim, 0];
+            ZValues[1, 1, 0] = _source.ZValues[xdim, ydim, 0];
+            
+            XValues[0, 1, 1] = _source.XValues[0, ydim, zdim];
+            YValues[0, 1, 1] = _source.YValues[0, ydim, zdim];
+            ZValues[0, 1, 1] = _source.ZValues[0, ydim, zdim];
+            
+            XValues[1, 0, 1] = _source.XValues[xdim, 0, zdim];
+            YValues[1, 0, 1] = _source.YValues[xdim, 0, zdim];
+            ZValues[1, 0, 1] = _source.ZValues[xdim, 0, zdim];
+            
+            XValues[1, 1, 1] = _source.XValues[xdim, ydim, zdim];
+            YValues[1, 1, 1] = _source.YValues[xdim, ydim, zdim];
+            ZValues[1, 1, 1] = _source.ZValues[xdim, ydim, zdim];
+            
+            UpdateDistances(Vector3.Zero);
+            
+        }
+
+        public void UpdateDistances(Vector3 eyePoint)
+        {
+            MinDist = double.MaxValue;
+            MaxDist = double.MinValue;
+            
+            for (var z = 0; z < 2; ++z)
+            {
+                for (var y = 0; y < 2; ++y)
+                {
+                    for (var x = 0; x < 2; ++x)
+                    {
+                        var dist = System.Math.Abs((new Vector3((float) _source.XValues[x, y, z],
+                            (float) _source.YValues[x, y, z], (float) _source.ZValues[x, y, z]) - eyePoint).Length());
+
+                        if (dist < MinDist) MinDist = dist;
+                        if (dist > MaxDist) MaxDist = dist;
+                        
+                        Values[x, y, z] = dist;
+                        
+                    }
+                }
+            }
+        }
+    }
     
+    class SamplingCullCallback : Callback, IDrawableCullCallback
+    {
+        private SampledVolumeNode _svn;
+        private IIsoSurfaceGenerator _isoSurfaceGenerator;
+        private SamplingVolume _samplingVolume;
+        public SamplingCullCallback(SampledVolumeNode svn)
+        {
+            _svn = svn;
+            _samplingVolume = new SamplingVolume(_svn.VoxelVolume);
+            _isoSurfaceGenerator = new MarchingCubesIsoSurfaceGenerator();
+        }
+        
+        public bool Cull(ICullVisitor cv, IDrawable drawable)
+        {
+            if (drawable is Geometry<Position3Color4> geometry)
+            {
+                _samplingVolume.UpdateDistances(-cv.GetEyeLocal());
+                Sample(geometry);
+                return false;
+            }
+            return true;
+        }
+
+        private void Sample(Geometry<Position3Color4> geometry)
+        {
+            var sampleRate = 10;
+            var sampleStep = (_samplingVolume.MaxDist - _samplingVolume.MinDist) / sampleRate;
+            geometry.PrimitiveSets.Clear();
+
+            List<IIsoSurface> isoSurfaces = new List<IIsoSurface>();
+            for (var i = _samplingVolume.MinDist; i <= _samplingVolume.MaxDist; i += sampleStep)
+            {
+                var isoSurface = _isoSurfaceGenerator.CreateIsoSurface(_samplingVolume, i);
+                isoSurfaces.Add(isoSurface);
+            }
+            
+            List<Position3Color4> sliceVertices = new List<Position3Color4>();
+            List<uint> indices = new List<uint>();
+            var idx = 0u;
+            
+            foreach (var isoSurface in isoSurfaces)
+            {
+                var startidx = idx;
+                var idxcount = 0u;
+
+                foreach (var vtx in isoSurface.IsoSurfaceVertices)
+                {
+                    sliceVertices.Add(new Position3Color4(new Vector3((float) vtx.X, (float) vtx.Y, (float) vtx.Z),
+                        new Vector4(0.0f, 0.0f, 1.0f, 0.5f)));
+                    indices.Add(idx++);
+                    ++idxcount;
+                }
+
+                if (0 != isoSurface.IsoSurfaceVertices.Count)
+                {
+                    var pSet = DrawElements<Position3Color4>.Create(
+                        geometry,
+                        PrimitiveTopology.TriangleList,
+                        idxcount,
+                        1,
+                        startidx,
+                        0,
+                        0);
+
+                    geometry.PrimitiveSets.Add(pSet);
+                }
+            }
+
+            if (0 == sliceVertices.Count) return;
+            
+            geometry.VertexData = sliceVertices.ToArray();
+            geometry.IndexData = indices.ToArray();
+        }
+    }
+
     public class SampledVolumeNode : Geode, ISampledVolumeNode
     {
-        public new static ISampledVolumeNode Create(IVoxelVolume voxelVolume)
+        private IVoxelVolume _voxelVolume;
+        public IVoxelVolume VoxelVolume => _voxelVolume;
+
+        public static ISampledVolumeNode Create(IVoxelVolume voxelVolume)
         {
             return new SampledVolumeNode(voxelVolume);
         }
 
         protected SampledVolumeNode(IVoxelVolume voxelVolume)
         {
+            _voxelVolume = voxelVolume;
+            
             CreateCube(voxelVolume);
             CreateSlices(voxelVolume);
         }
@@ -31,56 +201,17 @@ namespace Veldrid.SceneGraph.NodeKits.DirectVolumeRendering
 
         private void CreateSlices(IVoxelVolume voxelVolume)
         {
-            var isoSurfaceGenerator = new MarchingCubesIsoSurfaceGenerator();
-
-            List<IIsoSurface> isoSurfaces = new List<IIsoSurface>();
-            for (var i = 0; i < 40; ++i)
-            {
-                var isoSurface = isoSurfaceGenerator.CreateIsoSurface(voxelVolume, 0.25*i);
-                isoSurfaces.Add(isoSurface);
-            }
-            
-
-            List<Position3Color3> sliceVertices = new List<Position3Color3>();
-            List<uint> indices = new List<uint>();
-            var idx = 0u;
-            foreach (var isoSurface in isoSurfaces)
-            {
-                foreach (var vtx in isoSurface.IsoSurfaceVertices)
-                {
-                    sliceVertices.Add(new Position3Color3(new Vector3((float)vtx.X, (float)vtx.Y, (float)vtx.Z), Vector3.UnitX));
-                    indices.Add(idx++);
-                }
-            }
-
-
-            if (0 == sliceVertices.Count) return;
-            
-            var geometry = Geometry<Position3Color3>.Create();
-            
-            geometry.VertexData = sliceVertices.ToArray();
-            
-            geometry.IndexData = indices.ToArray();
+            var geometry = Geometry<Position3Color4>.Create();
 
             geometry.VertexLayouts = new List<VertexLayoutDescription>()
             {
-                Position3Color3.VertexLayoutDescription
+                Position3Color4.VertexLayoutDescription
             };
-
-            var pSet = DrawElements<Position3Color3>.Create(
-                geometry,
-                PrimitiveTopology.TriangleList,
-                (uint)indices.Count,
-                1,
-                0,
-                0,
-                0);
-
-            geometry.PrimitiveSets.Add(pSet);
-
-            geometry.PipelineState.ShaderSet = Position3Color3Shader.Instance.ShaderSet;
-            geometry.PipelineState.RasterizerStateDescription = RasterizerStateDescription.CullNone;
             
+            geometry.PipelineState.ShaderSet = Vertex3Color4Shader.Instance.ShaderSet;
+            geometry.PipelineState.BlendStateDescription = BlendStateDescription.SingleAlphaBlend;
+            geometry.PipelineState.RasterizerStateDescription = RasterizerStateDescription.CullNone;
+            geometry.SetCullCallback(new SamplingCullCallback(this));
             AddDrawable(geometry);
         }
         
---
+++ src/Veldrid.SceneGraph/VertexTypes/Position3Color4.cs
@@ -0,0 +1,79 @@
+//
+// This file is part of IMAGEFrac (R) and related technologies.
+//
+// Copyright (c) 2017-2020 Reveal Energy Services.  All Rights Reserved.
+//
+// LEGAL NOTICE:
+// IMAGEFrac contains trade secrets and otherwise confidential information
+// owned by Reveal Energy Services. Access to and use of this information is 
+// strictly limited and controlled by the Company. This file may not be copied,
+// distributed, or otherwise disclosed outside of the Company's facilities 
+// except under appropriate precautions to maintain the confidentiality hereof, 
+// and may not be used in any way not expressly authorized by the Company.
+//
+
+using System.Numerics;
+
+namespace Veldrid.SceneGraph.VertexTypes
+{
+    public struct Position3Color4 : ISettablePrimitiveElement
+    {
+        public Vector3 Position;
+        public Vector4 Color;
+        
+        public Position3Color4(Vector3 position, Vector4 color)
+        {
+            Position = position;
+            Color    = color;
+        }
+
+        public Vector3 VertexPosition
+        {
+            get => Position;
+            set => Position = value;
+        }
+
+        public static VertexLayoutDescription VertexLayoutDescription =>
+            
+            new VertexLayoutDescription(
+                new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate,
+                    VertexElementFormat.Float3),
+                new VertexElementDescription("Color", VertexElementSemantic.TextureCoordinate,
+                    VertexElementFormat.Float4));
+
+        public VertexLayoutDescription GetVertexLayoutDescription()
+        {
+            return VertexLayoutDescription;
+        }
+
+        public bool HasPosition => true;
+        public bool HasNormal => false;
+        public bool HasTexCoord => false;
+        public bool HasColor3 => false;
+        public bool HasColor4 => true;
+        public void SetPosition(Vector3 position)
+        {
+            Position = position;
+        }
+
+        public void SetNormal(Vector3 normal)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetTexCoord(Vector2 texCoord)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetColor3(Vector3 color)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public void SetColor4(Vector4 color)
+        {
+            Color = color;
+        }
+    }
+}
