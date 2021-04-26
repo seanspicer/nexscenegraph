06220f70936644d6fa7ec55353c0c47b733ed26c
Scott Senften <scott.senften@reveal-energy.com>
2021-04-26 17:09:37 -0500

    clean up

--- src/TexturedSlice3D/Program.cs
+++ src/TexturedSlice3D/Program.cs
@@ -40,75 +40,6 @@ namespace TexturedSlice3D
         {
             var geometry = Geometry<Position3TexCoord3Color4>.Create();
 
-#if false
-            var vertices = new[]
-            {
-                // Top
-                new Position3TexCoord3Color4(new Vector3(-1.0f, +1.0f, -1.0f), new Vector3(0, 1, 0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, +1.0f, -1.0f), new Vector3(1, 1, 0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, +1.0f, +1.0f), new Vector3(1, 1, 1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(-1.0f, +1.0f, +1.0f), new Vector3(0, 1, 1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                // Bottom                                                             
-                new Position3TexCoord3Color4(new Vector3(-1.0f, -1.0f, +1.0f), new Vector3(0, 0, 1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, -1.0f, +1.0f), new Vector3(1, 0, 1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, -1.0f, -1.0f), new Vector3(1, 0, 0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(-1.0f, -1.0f, -1.0f), new Vector3(0, 0, 0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                // Left                                                               
-                new Position3TexCoord3Color4(new Vector3(-1.0f, +1.0f, -1.0f), new Vector3(0, 1,0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(-1.0f, +1.0f, +1.0f), new Vector3(0, 1,1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(-1.0f, -1.0f, +1.0f), new Vector3(0, 0, 1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(-1.0f, -1.0f, -1.0f), new Vector3(0, 0, 0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                // Right                                                              
-                new Position3TexCoord3Color4(new Vector3(+1.0f, +1.0f, +1.0f), new Vector3(1, 1,1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, +1.0f, -1.0f), new Vector3(1, 1,0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, -1.0f, -1.0f), new Vector3(1, 0,0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, -1.0f, +1.0f), new Vector3(1, 0,1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                // Back                                                               
-                new Position3TexCoord3Color4(new Vector3(+1.0f, +1.0f, -1.0f), new Vector3(1, 1, 0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(-1.0f, +1.0f, -1.0f), new Vector3(0, 1, 0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(-1.0f, -1.0f, -1.0f), new Vector3(0, 0, 0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, -1.0f, -1.0f), new Vector3(1, 0, 0),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                // Front                                                              
-                new Position3TexCoord3Color4(new Vector3(-1.0f, +1.0f, +1.0f), new Vector3(0, 1, 1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, +1.0f, +1.0f), new Vector3(1, 1, 1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(+1.0f, -1.0f, +1.0f), new Vector3(1, 0, 1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f)),
-                new Position3TexCoord3Color4(new Vector3(-1.0f, -1.0f, +1.0f), new Vector3(0, 0, 1),
-                    new Vector4(0.0f, 0.0f, 1.0f, 1.0f))
-            };
-
-            uint[] indices =
-            {
-                0, 1, 2, 0, 2, 3,
-                4, 5, 6, 4, 6, 7,
-                8, 9, 10, 8, 10, 11,
-                12, 13, 14, 12, 14, 15,
-                16, 17, 18, 16, 18, 19,
-                20, 21, 22, 20, 22, 23
-            };
-#else
             var vertices = new[]
             {
                 // Y-Z Slice
@@ -146,7 +77,6 @@ namespace TexturedSlice3D
                 4, 5, 6, 4, 6, 7,
                 8, 9, 10, 8, 10, 11
             };
-#endif
             
             geometry.VertexData = vertices;
             geometry.IndexData = indices;
