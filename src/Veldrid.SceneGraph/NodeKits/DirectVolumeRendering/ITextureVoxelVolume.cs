66607dce8a62731436ab4ce33c73feb5d0164924
Scott Senften <scott.senften@reveal-energy.com>
2021-05-04 17:29:49 -0500

    remove ITextureVoxelVolume

--- src/Veldrid.SceneGraph/NodeKits/DirectVolumeRendering/ITextureVoxelVolume.cs
+++
@@ -1,9 +0,0 @@
-using Veldrid.SceneGraph.Math.IsoSurface;
-
-namespace Veldrid.SceneGraph.NodeKits.DirectVolumeRendering
-{
-    public interface ITextureVoxelVolume : IVoxelVolume
-    {
-        ITexture3D TextureData { get; }
-    }
-}
