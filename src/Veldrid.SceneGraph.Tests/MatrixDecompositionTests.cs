fd47f3aaa481c48e4c99fec0005663b552788a6b
seanspicer <sean.spicer@reveal-energy.com>
2021-05-14 22:41:06 -0500

    Add some unit tests.

--- src/Veldrid.SceneGraph.Tests/MatrixDecompositionTests.cs
+++ src/Veldrid.SceneGraph.Tests/MatrixDecompositionTests.cs
@@ -43,6 +43,50 @@ namespace Veldrid.SceneGraph.Tests
             Assert.That(so, Is.EqualTo(Quaternion.Identity));
         }
         
+        [TestCase]
+        public void PerformLeftHandedIdentityDecompositionSuccessfully()
+        {
+            Matrix4x4 mat = Matrix4x4.Identity;
+            mat = mat.PostMultiply(Matrix4x4.CreateScale(1, 1, -1));
+
+            mat.DecomposeAffine(out var scale, out var rotation, out var translation, out var so);
+
+            Assert.That(scale, Is.EqualTo(-1*Vector3.One));
+            Assert.That(translation, Is.EqualTo(Vector3.Zero));
+            Assert.That(rotation, Is.EqualTo(new Quaternion(0, 0, 1, 0)));
+            Assert.That(so, Is.EqualTo(Quaternion.Identity));
+        }
         
+        [TestCase]
+        public void PerformRotaionTranslationScaleDecompositionSuccessfully()
+        {
+            Matrix4x4 mat = Matrix4x4.Identity;
+            mat = mat.PostMultiply(Matrix4x4.CreateScale(2, 2, 2));
+            mat = mat.PostMultiply(Matrix4x4.CreateRotationX((float)System.Math.PI));
+            mat = mat.PostMultiply(Matrix4x4.CreateTranslation(1, 0 , 0));
+            
+            mat.DecomposeAffine(out var scale, out var rotation, out var translation, out var so);
+
+            Assert.That(scale, Is.EqualTo(2*Vector3.One));
+            Assert.That(translation, Is.EqualTo(Vector3.UnitX));
+            Assert.That(rotation, Is.EqualTo(new Quaternion(1, 0, 0, -4.371139E-08f)));
+            Assert.That(so, Is.EqualTo(Quaternion.Identity));
+        }
+        
+        [TestCase]
+        public void PerformLeftHandedRotaionTranslationScaleDecompositionSuccessfully()
+        {
+            Matrix4x4 mat = Matrix4x4.Identity;
+            mat = mat.PostMultiply(Matrix4x4.CreateScale(2, 2, -2));
+            mat = mat.PostMultiply(Matrix4x4.CreateRotationX((float)System.Math.PI));
+            mat = mat.PostMultiply(Matrix4x4.CreateTranslation(1, 0 , 0));
+            
+            mat.DecomposeAffine(out var scale, out var rotation, out var translation, out var so);
+
+            Assert.That(scale, Is.EqualTo(-2*Vector3.One));
+            Assert.That(translation, Is.EqualTo(Vector3.UnitX));
+            Assert.That(rotation, Is.EqualTo(new Quaternion(0, 1, 4.371139E-08f, 0)));
+            Assert.That(so, Is.EqualTo(Quaternion.Identity));
+        }
     }
 }
